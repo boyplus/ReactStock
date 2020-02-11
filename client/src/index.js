8f838acca354be4d27e7d9e6c3a115dccc2df8af
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2020-02-11 14:50:46 +0700

    [ADD] actions and reducers

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleWare, compose } from 'redux';
+import reduxThunk from 'redux-thunk';
 import App from './components/App';
 
-ReactDOM.render(
-    <App />, document.querySelector('#root')
-);
+
+ReactDOM.render(<App />, document.querySelector('#root'));
