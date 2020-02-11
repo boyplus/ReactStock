9ff45fa0193f7bc7ca50ea3ccdd1beb36058e3bb
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2020-02-11 15:18:15 +0700

    [ADD] react-router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
-
+import { Router, Route } from 'react-router-dom';
+import history from '../history';
+import Header from './Header';
+import Home from './Home';
+import Stocks from './Stocks';
+import Portfolio from './Portfolio';
 const App = () => {
     return (
-        <div>Welcome to React Stock</div>
+        <div className="ui container">
+            <Router history={history}>
+                <Header></Header>
+                <div>
+                    <Route path="/" exact component={Home}></Route>
+                    <Route path="/stocks" exact component={Stocks}></Route>
+                    <Route
+                        path="/portfolio"
+                        exact
+                        component={Portfolio}
+                    ></Route>
+                </div>
+            </Router>
+        </div>
     );
 };
-
 export default App;
---
+++ client/src/components/Stocks.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Stocks = () => {
+    return (
+        <div>
+            <div className="ui header">Stocks list</div>
+        </div>
+    );
+};
+
+export default Stocks;
