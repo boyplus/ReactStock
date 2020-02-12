71a3426ea41a87d072576b6b055a4e1555a9725c
Mr.SETHANANT PIPATPAKORN <62130500230@st.sit.kmutt.ac.th>
2020-02-12 21:14:55 +0700

    [ADD] cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const userRouter = require('./routes/user')
 const stockRouter = require('./routes/stock')
 
 const app = express()
 
+app.use(cors())
 app.use(userRouter)
 app.use(stockRouter)
 
