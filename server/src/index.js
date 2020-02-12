d8dce3efbfe933a85142be54d8374c2cc6ec9764
Mr.SETHANANT PIPATPAKORN <62130500230@st.sit.kmutt.ac.th>
2020-02-12 20:35:56 +0700

    [FIX] port

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3030
 app.listen(port, () => {
 	if (process.env.NODE_ENV != 'production') {
 		console.log(`Running on ${port}`)
