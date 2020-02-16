282a1c1479edf93699fd79569e1ddbe1ecf1fe7a
Mr.SETHANANT PIPATPAKORN <62130500230@st.sit.kmutt.ac.th>
2020-02-16 20:48:33 +0700

    [FIX] PUT CORS policy

--- server/src/app.js
+++ server/src/app.js
@@ -24,7 +24,7 @@ app.use(
 	cors({
 		origin: 'http://localhost:3000',
 		credentials: true,
-		methods: ['GET', 'POST', 'PATCH'],
+		methods: ['GET', 'POST', 'PUT', 'PATCH'],
 		// allowedHeaders: ['Content-Type', 'Authorization'],
 	})
 )
